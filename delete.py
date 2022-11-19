7da25c208d350d371fa5566a791c8d91bf3695aa
Panshul Gupta <86116744+PanshulGupta@users.noreply.github.com>
2022-11-19 08:51:02 +0400

    Add files via upload

---
+++ delete.py
@@ -0,0 +1,10 @@
+
+import pandas as pd
+import csv
+df = pd.read_csv('Total Stars.csv')
+del df['Luminosity']
+del df['Star_name']
+del df['Distance']
+del df['Mass']
+del df['Radius']
+df.to_csv('Final.csv')
